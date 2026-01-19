c2ef614c18c5b8aa4d08a6b46ec4a048b07fbda6
Medifff <34546235+Medifff@users.noreply.github.com>
2026-01-19 19:11:23 +0200

    Update radio.py

--- radio.py
+++ radio.py
@@ -231,16 +231,16 @@ def streamer():
     while video_queue.empty():
         time.sleep(2)
 
-     cmd = [
+    cmd = [
         "ffmpeg",
-        "-ignore_eof", "1",              
+        "-ignore_eof", "1",
         "-fflags", "+genpts+igndts",
         "-f", "mpegts",
         "-i", "pipe:0",
-    
+
         "-vsync", "cfr",
         "-r", "30",
-    
+
         "-c:v", "libx264",
         "-preset", "veryfast",
         "-tune", "zerolatency",
@@ -250,13 +250,13 @@ def streamer():
         "-b:v", "3000k",
         "-maxrate", "3000k",
         "-bufsize", "6000k",
-    
+
         "-c:a", "aac",
         "-ar", "44100",
         "-ac", "2",
         "-b:a", "160k",
         "-af", "aresample=async=1:first_pts=0",
-    
+
         "-f", "flv",
         RTMP_URL
     ]
